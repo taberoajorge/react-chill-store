afbf0c3604db9c9154d7ec929c2c546d06d606b7
Jorge Alexander Taberoa Jimenez <taberoajorge@pm.me>
2021-12-23 17:03:36 -0600

    Se configura entorno local

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = function App() {
+  return (
+    <div> Hello World </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(
+  // eslint-disable-next-line react/jsx-filename-extension
+  <App />,
+  // eslint-disable-next-line no-undef
+  document.getElementById('root'),
+);
